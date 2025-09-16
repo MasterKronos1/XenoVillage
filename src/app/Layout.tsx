133b3304f33836aa5ccf976270f18f2c83391ac9
MasterKronos1 <44232109+MasterKronos1@users.noreply.github.com>
2025-09-16 22:05:50 +1200

    Create Layout.tsx

---
+++ src/app/Layout.tsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+import { ReactNode } from "react";
+
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+return (
+<html lang="en">
+<body className="bg-gray-900 text-white font-sans">{children}</body>
+</html>
+);
+}
